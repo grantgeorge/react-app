4cc043bbb042f854a7ab7af88bcdd86724da1408
Grant George <grantelliottgeorge@gmail.com>
2015-07-18 17:48:12 -0400

    remove docs from sauce

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -12,13 +12,16 @@ class ContactPage {
   };
 
   render() {
-    let title = 'Contact Us';
+    let title = 'Contact Me';
     this.context.onSetTitle(title);
     return (
       <div className="ContactPage">
         <div className="ContactPage-container">
           <h1>{title}</h1>
-          <p>...</p>
+          <p>Grant George</p>
+          <p>Email: grant [at] grantgeorge [dot] io</p>
+          <p>GitHub: https://github.com/grantgeorge</p>
+          <p>StackOverflow: http://stackoverflow.com/users/2403277/grant</p>
         </div>
       </div>
     );
