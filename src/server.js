51d6a73525dc9f2be12a863f53983be7e7a7a975
Grant George <grantelliottgeorge@gmail.com>
2015-07-18 16:57:39 -0400

    drup

--- src/server.js
+++ src/server.js
@@ -31,14 +31,10 @@ const template = _.template(fs.readFileSync(templateFile, 'utf8'));
 
 server.get('*', async (req, res, next) => {
   try {
-<<<<<<< HEAD
     // TODO: Temporary fix #159
     if (['/', '/about', '/privacy'].indexOf(req.path) !== -1) {
       await db.getPage(req.path);
     }
-=======
-    let uri = req.path;
->>>>>>> init
     let notFound = false;
     let css = [];
     let data = {description: ''};
@@ -51,10 +47,6 @@ server.get('*', async (req, res, next) => {
         onPageNotFound: () => notFound = true
       }} />);
 
-<<<<<<< HEAD
-=======
-    await db.getPage(uri);
->>>>>>> init
     data.body = React.renderToString(app);
     data.css = css.join('');
     let html = template(data);
